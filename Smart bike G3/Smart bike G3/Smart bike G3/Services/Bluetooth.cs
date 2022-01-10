7c0f2816639d3fa9c26367548a4baf0bcf6206ca
Wolf Nemegeer <wolf.nemegeer@outlook.com>
2022-01-10 21:42:25 +0100

    clean up classe bluethoot

--- Smart bike G3/Smart bike G3/Smart bike G3/Services/Bluetooth.cs
+++ Smart bike G3/Smart bike G3/Smart bike G3/Services/Bluetooth.cs
@@ -26,25 +26,7 @@ namespace Smart_bike_G3.Services
 
         public  void Scan()
         {
-            //scanCallback.OnScanResult(ScanCallbackType.AllMatches, null);
-
-            // scanner = adapter.BluetoothLeScanner;
-            //scanner.StartScan(scanCallback);
-
-            //adapter.StartLeScan(null);
-            //adapter.StartDiscovery();
-            
-
-            //BluetoothDevice device = (from bd in adapter.BondedDevices
-            //                          where bd.Name == "NameOfTheDevice"
-            //                          select bd).FirstOrDefault();
-
-            //if (device == null)
-            //    throw new Exception("Named device not found.");
-
-
-
-
+           
         }
 
 
@@ -52,85 +34,6 @@ namespace Smart_bike_G3.Services
         public async Task Connect(int index)
         {
 
-  /**
-                BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
-                if (adapter == null) Debug.WriteLine("No Bluetooth adapter found.");
-                else if (!adapter.IsEnabled) Debug.WriteLine("Bluetooth adapter is not enabled.");
-
-                List<BluetoothDevice> L = new List<BluetoothDevice>();
-                foreach (BluetoothDevice d in adapter.BondedDevices)
-                {
-                    Debug.WriteLine("D: " + d.Name + " " + d.Address + " " + d.BondState.ToString());
-                    L.Add(d);
-                }
-
-                //BluetoothDevice device = null;
-                //device = L.Find(j => j.Name == "PC-NAME");
-                var device = listDevices[index];
-                var u = device.GetUuids()[0];
-
-                if (device == null) Debug.WriteLine("Named device not found.");
-                else
-                {
-                    Debug.WriteLine("Device has been found: " + device.Name + " " + device.Address + " " + device.BondState.ToString());
-                }
-
-                _socket = device.CreateRfcommSocketToServiceRecord(u.Uuid);
-                await _socket.ConnectAsync();
-
-                if (_socket != null && _socket.IsConnected) Debug.WriteLine("Connection successful!");
-                else Debug.WriteLine("Connection failed!");
-
-                var inStream = (InputStreamInvoker)_socket.InputStream;
-                var outStream = (OutputStreamInvoker)_socket.OutputStream;
-
-
-                if (_socket != null && _socket.IsConnected)
-                {
-                    Task t = new Task(() => Listen(inStream));
-                    t.Start();
-                }
-                else throw new Exception("Socket not existing or not connected.");
-            }
-
-            private static async void Listen(Stream inStream)
-            {
-                bool Listening = true;
-                Debug.WriteLine("Listening has been started.");
-                byte[] uintBuffer = new byte[sizeof(uint)]; // This reads the first 4 bytes which form an uint that indicates the length of the string message.
-                byte[] textBuffer; // This will contain the string message.
-
-                // Keep listening to the InputStream while connected.
-                while (Listening)
-                {
-                    try
-                    {
-                        // This one blocks until it gets 4 bytes.
-                        await inStream.ReadAsync(uintBuffer, 0, uintBuffer.Length);
-                        uint readLength = BitConverter.ToUInt32(uintBuffer, 0);
-
-                        textBuffer = new byte[readLength];
-                        // Here we know for how many bytes we are looking for.
-                        await inStream.ReadAsync(textBuffer, 0, (int)readLength);
-
-                        string s = Encoding.UTF8.GetString(textBuffer);
-                        Debug.WriteLine("Received: " + s);
-                    }
-                    catch (Java.IO.IOException e)
-                    {
-                        Debug.WriteLine("Error: " + e.Message);
-                        Listening = false;
-                        break;
-                    }
-                }
-                Debug.WriteLine("Listening has ended.");
-            }
-
-            BluetoothDevice device = (from bd in adapter.BondedDevices
-                                      where bd.Name == "NameOfTheDevice"
-                                      select bd).FirstOrDefault();
-            
-            */
             try
             {
                 if (adapter == null)
@@ -163,16 +66,7 @@ namespace Smart_bike_G3.Services
 
 
 
-        //public async Task<int> getdate(IDevice device = null)
-        //{
-        //    if(device == null)
-        //    {
-        //        device = BluetoothAdapter.ConnectedDevices.First();
-        //        Characteristics[0].StartUpdates();
-        //        return Characteristics[0].Value();
-        //    }
-
-        //}
+        
 
         public List<string> GetDevices()
         {
@@ -198,40 +92,10 @@ namespace Smart_bike_G3.Services
             return message;
          
 
-            //using (var rawdata3 = _socket.InputStream)
-            //{
-            //    var _ist = (rawdata3 as InputStreamInvoker).BaseInputStream;
-            //    var aa = 0;
-            //    if ((aa = _ist.Available()) > 0)
-            //    {
-            //        var nn = _ist.Read(buffer, 0, aa);
-            //        System.Array.Resize(ref buffer, nn);
-            //    }
-            //}
-            return "12";
-            //int rawdata2 = await (Stream)_socket.InputStream;
-
-
-
-            //using (StreamReader reader = new StreamReader(rawdata.))
-            //string data = rawdata.
+           
         }
 
 
-        //private static void DeviceDiscovered(object sender, DeviceDiscoveredEventArgs e) // --> scan
-        //{
-        //    if (_deviceList.All(x => x.Id != e.Device.Id))
-        //    {
-        //        _deviceList.Add(e.Device);
-        //    }
-        //}
-
-        //private static void DeviceDiscovered(object sender, DeviceDiscoveredEventArgs e) // --> scan
-        //{
-        //    if (_deviceList.All(x => x.Id != e.Device.Id))
-        //    {
-        //        _deviceList.Add(e.Device);
-        //    }
-        //}
+       
     }
 }
