610c246c3521f4148a9b63bc06f2d036689897c5
EechauteAntje <antje.eechaute@student.howest.be>
2022-01-11 15:55:37 +0100

    commit

--- Smart bike G3/Smart bike G3/Smart bike G3/App.xaml.cs
+++ Smart bike G3/Smart bike G3/Smart bike G3/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Smart_bike_G3.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace Smart_bike_G3
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Name());
         }
 
         protected override void OnStart()
---
+++ Smart bike G3/Smart bike G3/Smart bike G3/Views/VideoOrGame.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smart_bike_G3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VideoOrGame : ContentPage
+    {
+        public VideoOrGame()
+        {
+            InitializeComponent();
+            btnGame.Clicked += BtnGame_Clicked;
+            btnVideo.Clicked += BtnVideo_Clicked;
+            Images();
+        }
+
+        private void Images()
+        {
+            imgVideo.Source = ImageSource.FromResource(@"Smart_bike_G3.Assets.video.png");
+            imgGame.Source = ImageSource.FromResource(@"Smart_bike_G3.Assets.Game.png");
+        }
+
+        private void BtnVideo_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ChooseVideo());
+        }
+
+        private void BtnGame_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
