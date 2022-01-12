6e1996e6c16de386d3a2635095f5a06145232bd4
Laura Wittevrongel <laura.wittevrongel@student.howest.be>
2022-01-12 16:45:05 +0100

    Error fixed

--- Smart bike G3/Smart bike G3/Smart bike G3/Views/ChooseVideo.xaml.cs
+++ Smart bike G3/Smart bike G3/Smart bike G3/Views/ChooseVideo.xaml.cs
@@ -15,12 +15,36 @@ namespace Smart_bike_G3.Views
         public ChooseVideo()
         {
             InitializeComponent();
-            Images();
+            AddEvents();
         }
-        private void Images()
+        private void AddEvents()
         {
-            imgLuisterboek.Source = ImageSource.FromResource(@"Smart_bike_G3.Assets.Luisterboek.png");
-            imgMuziek.Source = ImageSource.FromResource(@"Smart_bike_G3.Assets.Muziek.png");
+            TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += AbsLayBack_Tabbed;
+            AbsLayBack.GestureRecognizers.Add(tapGestureRecognizer);
+
+            TapGestureRecognizer tapGestureRecognizer2 = new TapGestureRecognizer();
+            tapGestureRecognizer2.Tapped += AbsLayMusic_Tabbed;
+            AbsLayMusic.GestureRecognizers.Add(tapGestureRecognizer2);
+
+            TapGestureRecognizer tapGestureRecognizer3 = new TapGestureRecognizer();
+            tapGestureRecognizer3.Tapped += AbsLayBook_Tabbed;
+            AbsLayBook.GestureRecognizers.Add(tapGestureRecognizer3);
+        }
+
+        private void AbsLayBook_Tabbed(object sender, EventArgs e)
+        {
+            //Console.WriteLine("tabbed book");
+        }
+
+        private void AbsLayMusic_Tabbed(object sender, EventArgs e)
+        {
+            //Console.WriteLine("tabbed music");
+        }
+
+        private void AbsLayBack_Tabbed(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
         }
     }
 }
--- Smart bike G3/Smart bike G3/Smart bike G3/Views/videoScorebord.xaml.cs
+++ Smart bike G3/Smart bike G3/Smart bike G3/Views/videoScorebord.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Smart_bike_G3.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
