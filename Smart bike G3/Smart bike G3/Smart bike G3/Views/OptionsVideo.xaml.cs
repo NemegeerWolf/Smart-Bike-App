4f22f650b619155589e2a9b77cbbc4461da384ab
EechauteAntje <antje.eechaute@student.howest.be>
2022-01-13 13:32:50 +0100

    optie pagina verschillende videos

--- Smart bike G3/Smart bike G3/Smart bike G3/Views/OptionsVideo.xaml.cs
+++ Smart bike G3/Smart bike G3/Smart bike G3/Views/OptionsVideo.xaml.cs
@@ -12,9 +12,39 @@ namespace Smart_bike_G3.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class OptionsVideo : ContentPage
     {
-        public OptionsVideo()
+        public string Kind { get; set; }
+        public OptionsVideo(string kind)
         {
             InitializeComponent();
+            Kind = kind;
+            btnFirst.Clicked += BtnFirst_Clicked;
+            btnSecond.Clicked += BtnSecond_Clicked;
+            btnThird.Clicked += BtnThird_Clicked;
+            btnFourth.Clicked += BtnFourth_Clicked;
+        }
+
+        private void BtnFourth_Clicked(object sender, EventArgs e)
+        {
+            string kind = Kind;
+            Navigation.PushAsync(new ChooseVideo(kind));
+        }
+
+        private void BtnThird_Clicked(object sender, EventArgs e)
+        {
+            string kind = Kind;
+            Navigation.PushAsync(new ChooseVideo(kind));
+        }
+
+        private void BtnSecond_Clicked(object sender, EventArgs e)
+        {
+            string kind = Kind;
+            Navigation.PushAsync(new ChooseVideo(kind));
+        }
+
+        private void BtnFirst_Clicked(object sender, EventArgs e)
+        {
+            string kind = Kind;
+            Navigation.PushAsync(new ChooseVideo(kind));
         }
     }
 }
