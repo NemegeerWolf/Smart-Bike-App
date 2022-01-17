9762aaf154efe1d3a9782920bfadae907c5477d0
EechauteAntje <antje.eechaute@student.howest.be>
2022-01-17 16:10:22 +0100

    video uitleg pagina

--- Smart bike G3/Smart bike G3/Smart bike G3/Views/OptionsVideo.xaml.cs
+++ Smart bike G3/Smart bike G3/Smart bike G3/Views/OptionsVideo.xaml.cs
@@ -92,25 +92,25 @@ namespace Smart_bike_G3.Views
         {
             
             VideoId = 4;
-            Navigation.PushAsync(new VideoPage());
+            Navigation.PushAsync(new VideoExplanation());
         }
 
         private void BtnThird_Clicked(object sender, EventArgs e)
         {
             VideoId= 3;
-            Navigation.PushAsync(new VideoPage());
+            Navigation.PushAsync(new VideoExplanation());
         }
 
         private void BtnSecond_Clicked(object sender, EventArgs e)
         {
             VideoId = 2;
-            Navigation.PushAsync(new VideoPage());
+            Navigation.PushAsync(new VideoExplanation());
         }
 
         private void BtnFirst_Clicked(object sender, EventArgs e)
         {
             VideoId = 1;
-            Navigation.PushAsync(new VideoPage());
+            Navigation.PushAsync(new VideoExplanation());
         }
 
         private void AbsLayBack_Tabbed(object sender, EventArgs e)
