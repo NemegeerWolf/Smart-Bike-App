ccc62e71d96def8b1bac96ed40ee1b2d918449eb
Wolf Nemegeer <wolf.nemegeer@outlook.com>
2022-01-23 20:22:34 +0100

    bluethoot and sensor working (mvp)

--- Smart bike G3/Smart bike G3/Smart bike G3.Android/MainActivity.cs
+++ Smart bike G3/Smart bike G3/Smart bike G3.Android/MainActivity.cs
@@ -5,14 +5,54 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 
+using Android;
+using Android.Bluetooth;
+using Android.Content;
+using Android.Support.V4.App;
+using Smart_bike_G3.Models;
+using AndroidX.Core.Content;
+using AndroidX.Core.App;
+
 namespace Smart_bike_G3.Droid
 {
     [Activity(Label = "Smart_bike_G3", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private BluetoothDeviceReceiver _receiver;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            const int locationPermissionsRequestCode = 1000;
+
+            var locationPermissions = new[]
+            {
+                Manifest.Permission.AccessCoarseLocation,
+                Manifest.Permission.AccessFineLocation
+            };
+
+            // check if the app has permission to access coarse location
+            var coarseLocationPermissionGranted =
+                ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation);
+
+            // check if the app has permission to access fine location
+            var fineLocationPermissionGranted =
+                ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation);
+
+            // if either is denied permission, request permission from the user
+            if (coarseLocationPermissionGranted == Permission.Denied ||
+                fineLocationPermissionGranted == Permission.Denied)
+            {
+                ActivityCompat.RequestPermissions(this, locationPermissions, locationPermissionsRequestCode);
+            }
+
+            // Register for broadcasts when a device is discovered
+            _receiver = new BluetoothDeviceReceiver();
+
+            RegisterReceiver(_receiver, new IntentFilter(BluetoothDevice.ActionFound));
+            
+
+            BluetoothDeviceReceiver.Adapter.StartDiscovery();
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
